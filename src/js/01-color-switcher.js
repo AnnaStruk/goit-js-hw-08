a53ce4c63e6ac8ebf8ab2fbb2ef731d23fceb329
Anna Struk <67921208+AnnaStruk@users.noreply.github.com>
2023-05-04 21:06:53 +0200

    Adds code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+const startButton = document.getElementById("start-btn");
+const stopButton = document.getElementById("stop-btn");
 
+startButton.addEventListener("click", startBackgroundChange);
+stopButton.addEventListener("click", stopBackgroundChange);
+
+let intervalId;
+
+function startBackgroundChange() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  intervalId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopBackgroundChange() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+/* Another way */
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+//   }
+
+//   let intervalId;
+//   const startBtn = document.querySelector('[data-start]');
+//   const stopBtn = document.querySelector('[data-stop]');
+
+//   startBtn.addEventListener('click', () => {
+//     startBtn.disabled = true;
+//     stopBtn.disabled = false;
+
+//     intervalId = setInterval(() => {
+//       document.body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//   });
+
+//   stopBtn.addEventListener('click', () => {
+//     clearInterval(intervalId);
+//     startBtn.disabled = false;
+//     stopBtn.disabled = true;
+//   });
